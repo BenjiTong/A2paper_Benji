6a18b22fa0587155bf68cfeabaef56b70f286e4c
Zhen Chen <cc0cc@126.com>
2021-08-19 23:49:08 +0800

    fix jsp

--- Java/src/main/java/com/light/controllers/HelloWorldController.java
+++ Java/src/main/java/com/light/controllers/HelloWorldController.java
@@ -1,17 +1,19 @@
-package com.light.web;
+package com.light.controllers;
 
 import java.util.Map;
  
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HelloWorldController {
 	private String message = "Hello World";
  
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "HelloWorld.jsp";
+		return "HelloWorld";
 	}
 }
