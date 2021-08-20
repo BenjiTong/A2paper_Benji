7d5f23af67a221142d650a63bd8e0814e209cfb8
JeffXu <jeffxu717@hotmail.com>
2021-08-20 14:54:49 +0800

    1. set eslint configue 2. add instructions to README.md

--- Frontend/README.md
+++ Frontend/README.md
@@ -2,6 +2,43 @@
 
 > frontend part of a2 project
 
+## Set environment
+
+1. Node 16.7.0
+2. Vue 2.X
+
+```
+Windows 下的 VM 官方网址： https: //github.com coreybutler nvm-windows  
+nvm根目录下settings.txt添加（从国内镜像安装）  
+node_mirror: http://npm.taobao.org/mirrors/node/  
+npm_mirror: https://npm.taobao.org/mirrors/npm/
+```
+
+```bash
+# install node
+ηvm install 16.7.0
+
+# use node
+nvm use 16.7.0
+
+# install cpn to replace npm
+npm install - g cnpm --registry=https//registry.npm.taobao.com
+
+# install vue cli
+cnpm install vue-cli -g
+```
+
+## Run Frontend
+
+```bash
+# install modules
+cd Frontend  
+cnpm nstall
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
+
 ## Build Setup
 
 ``` bash
--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -1,90 +1,98 @@
 <template>
-  <div class="row lyear-wrapper">
-    <div class="lyear-login">
-      <div class="login-center">
-        <div class="form-group has-feedback feedback-left">
-          <input type="text" placeholder="Your email" v-model="email" />
-          <span
-            class="mdi mdi-account form-control-feedback"
-            aria-hidden="true"
-          ></span>
+    <div class="row lyear-wrapper">
+        <div class="lyear-login">
+            <div class="login-center">
+                <div class="form-group has-feedback feedback-left">
+                    <input
+                        type="text"
+                        placeholder="Your email"
+                        v-model="email"
+                    />
+                    <span
+                        class="mdi mdi-account form-control-feedback"
+                        aria-hidden="true"
+                    ></span>
+                </div>
+                <div class="form-group has-feedback feedback-left">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        v-model="password"
+                    />
+                    <span
+                        class="mdi mdi-lock form-control-feedback"
+                        aria-hidden="true"
+                    ></span>
+                </div>
+                <div class="form-group">
+                    <input
+                        class="btn btn-block btn-primary"
+                        type="button"
+                        @click="submit"
+                        value="Login"
+                    />
+                </div>
+                <hr />
+                <footer class="col-sm-12 text-center">
+                    <p class="m-b-0">{{ copyright }}</p>
+                </footer>
+            </div>
         </div>
-        <div class="form-group has-feedback feedback-left">
-          <input type="password" placeholder="Password" v-model="password" />
-          <span
-            class="mdi mdi-lock form-control-feedback"
-            aria-hidden="true"
-          ></span>
-        </div>
-        <div class="form-group">
-          <input
-            class="btn btn-block btn-primary"
-            type="button"
-            @click="submit"
-            value="Login"
-          />
-        </div>
-        <hr />
-        <footer class="col-sm-12 text-center">
-          <p class="m-b-0">{{ copyright }}</p>
-        </footer>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      email: '',
-      password: '',
-      copyright: 'Copyright © 2021 A2Inc. All right reserved'
+    data: function () {
+        return {
+            email: '',
+            password: '',
+            copyright: 'Copyright © 2021 A2Inc. All right reserved'
+        }
+    },
+    methods: {
+        submit: function () {
+            // Http request here
+        }
     }
-  },
-  methods: {
-    submit: function () {
-      // Http request here
-    }
-  }
 }
 </script>
 
 <style scoped>
 .lyear-wrapper {
-  position: relative;
+    position: relative;
 }
 .lyear-login {
-  display: flex !important;
-  min-height: 100vh;
-  align-items: center !important;
-  justify-content: center !important;
+    display: flex !important;
+    min-height: 100vh;
+    align-items: center !important;
+    justify-content: center !important;
 }
 .login-center {
-  /* background: #fff; */
-  background: rgba(255, 255, 255, 0.5);
-  min-width: 38.25rem;
-  padding: 2.14286em 3.57143em;
-  border-radius: 5px;
-  margin: 2.85714em 0;
+    /* background: #fff; */
+    background: rgba(255, 255, 255, 0.5);
+    min-width: 38.25rem;
+    padding: 2.14286em 3.57143em;
+    border-radius: 5px;
+    margin: 2.85714em 0;
 }
 .login-header {
-  margin-bottom: 1.5rem !important;
+    margin-bottom: 1.5rem !important;
 }
 .login-center .has-feedback.feedback-left .form-control {
-  padding-left: 38px;
-  padding-right: 12px;
+    padding-left: 38px;
+    padding-right: 12px;
 }
 .login-center .has-feedback.feedback-left .form-control-feedback {
-  left: 0;
-  right: auto;
-  width: 38px;
-  height: 38px;
-  line-height: 38px;
-  z-index: 4;
-  color: #dcdcdc;
+    left: 0;
+    right: auto;
+    width: 38px;
+    height: 38px;
+    line-height: 38px;
+    z-index: 4;
+    color: #dcdcdc;
 }
 .login-center .has-feedback.feedback-left.row .form-control-feedback {
-  left: 15px;
+    left: 15px;
 }
 </style>
