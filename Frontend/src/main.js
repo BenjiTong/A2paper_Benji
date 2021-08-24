06f36a6bb69ed4cd15a27c9b2d1f1d73cd8167fa
JeffXu <jeffxu717@hotmail.com>
2021-08-24 19:10:27 +0800

    1.add a page to test d3. 2.a new login page

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,12 +4,26 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import '@/assets/css/socicon.css'
+import '@/assets/css/bootstrap.css'
+import '@/assets/css/stack-interface.css'
+import '@/assets/css/theme-serpent.css'
+import '@/assets/css/custom.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 
+function fakeAuth () {
+    if (sessionStorage['token'] != null) {
+        return true
+    } else {
+        return false
+    }
+}
+
 router.beforeEach((to, from, next) => {
-    if (to.meta.requireAuth) {
+    if (to.meta.requireAuth && !fakeAuth) {
         // Check login state
         next({ path: '/login' })
     } else {
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Login from '@/components/Login'
+import TestD3 from '@/components/TestD3'
 
 Vue.use(Router)
 
@@ -19,6 +20,11 @@ export default new Router({
             path: '/login',
             name: 'Login',
             component: Login
+        },
+        {
+            path: '/test_d3',
+            name: 'TestD3',
+            component: TestD3
         }
     ]
 })
