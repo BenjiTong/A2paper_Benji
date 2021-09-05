96ecdd451c3c42e05b14c4b3626172c5c967fed1
54chen <czhttp@gmail.com>
2021-09-05 14:28:21 +0800

    token

--- Java/src/main/java/com/light/Util/GithubConstant.java
+++ Java/src/main/java/com/light/Util/GithubConstant.java
@@ -4,13 +4,11 @@ public class GithubConstant {
 
     public static final String CLIENT_ID="c04fa22c7aa981ba6419";
 
-    public static final String CLIENT_SECRET="bdb66391bf8b1eff8f18fc84357cfabedfe03e4c";
-
     public static final String CALLBACK = "http://localhost:8888/oauth/redirect";
 
     public static final String CODE_URL = "https://github.com/login/oauth/authorize?client_id="+CLIENT_ID+"state=STATE&redirect_uri="+CALLBACK+"";
 
-    public static final String TOKEN_URL = "https://github.com/login/oauth/access_token?client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&code=CODE&redirect_uri="+CALLBACK+"";
+    public static final String TOKEN_URL = "https://github.com/login/oauth/access_token?client_id="+CLIENT_ID+"&client_secret=CLIENT_SECRET&code=CODE&redirect_uri="+CALLBACK+"";
 
     public static final String USER_INFO_URL = "https://api.gihub.com/user/repos?access_token=TOKEN";
 
